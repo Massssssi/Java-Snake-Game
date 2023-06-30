8a9ce378671074d34335f2bf594320724b27d04d
amazigh Bouldjenet <sc20amb@leeds.ac.uk>
2023-06-30 11:33:40 +0100

    added apple and moving snake body to the grid

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -46,24 +46,63 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
 
-    public void painComponent(Graphics g)
+    public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
+        draw(g);
     }
 
-    public void draw(ActionEvent e)
-    {
+    public void draw(Graphics g) {
+        for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
 
+        for(int i = 0; i< bodyParts; i++)
+        {
+            if(i == 0)
+            {
+                g.setColor(Color.green);
+            }
+            else
+            {
+                g.setColor(new Color(45,180,0));
+            }
+            g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+
+        }
     }
 
     public void newApple(){
-
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
 
     public void move()
     {
+        for(int i = bodyParts; i>0;i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+            switch(direction)
+            {
+                case 'U':
+                    y[0] = y[0] - UNIT_SIZE;
+                    break;
+                case 'D':
+                    y[0] = y[0] + UNIT_SIZE;
+                    break;
+                case 'L':
+                    x[0] = x[0] - UNIT_SIZE;
+                    break;
+                case 'R':
+                    x[0] = x[0] + UNIT_SIZE;
+                    break;
 
-    }
+            }
+        }
 
     public void checkApple()
     {
@@ -72,7 +111,35 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void checkCollision()
     {
+        for(int i = bodyParts; i>0; i--)
+        {
+            if((x[0] == x[i]) && (y[0] == y[i]))
+            {
+                running = false;
+            }
+            if(x[0] < 0) {
+                running = false;
+            }
+            if(x[0] >SCREEN_WIDTH)
+            {
+                running = false;
+            }
 
+            if(y[0] >0)
+            {
+                running = false;
+            }
+
+            if(y[0] >SCREEN_HEIGHT)
+            {
+                running = false;
+            }
+
+            if(!running)
+            {
+                timer.stop();
+            }
+        }
     }
 
     public void gameOver(Graphics g)
@@ -91,5 +158,11 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if(running){
+            move();
+            checkApple();
+            checkCollision();
+        }
+        repaint();
     }
 }
